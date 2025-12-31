b8de848ce0a1f113e833792df4d1348b136a46fd
Gina Kong <ginagkong@gmail.com>
2025-12-31 12:46:00 +1100

    Setup react

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import dearDiaryLogo from './assets/dear-diary-shadow-logo.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://github.com/gina-kong/dear-diary" target="_blank">
+          <img src={dearDiaryLogo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>Dear Diary...</h1>
+    </>
+  )
+}
+
+export default App
